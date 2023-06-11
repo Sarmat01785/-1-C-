45a97fce6b580759c48c63aaef3ddc6c5c6d71a2
Sarmat <Spirator@Yandex.ru>
2023-06-12 06:49:49 +0700

    Решил задачу

--- Mission_1/Program.cs
+++ Mission_1/Program.cs
@@ -3,4 +3,17 @@
 // a = 2; b = 10 -> max = 10
 // a = -9; b = -3 -> max = -3
 
-
+Console.Write("Введите первое целое число:");
+string number = Console.ReadLine()!;
+int a = int.Parse(number);
+Console.Write("Введите второе целое число:");
+number = Console.ReadLine()!;
+int b = int.Parse(number);
+int max = a;
+if (a > max) max = a;
+else
+{
+    if (b > max) max = b;
+}
+Console.WriteLine(max);
+    
