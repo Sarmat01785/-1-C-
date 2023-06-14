8bc0d7e1a7d8c8133eba766049aa1ee3f5258b4d
Sarmat <Spirator@Yandex.ru>
2023-06-14 22:53:47 +0700

    адача 3 решена

--- Mission_3/Program.cs
+++ Mission_3/Program.cs
@@ -1,2 +1,8 @@
 ﻿// Написать программу которая на вход принемает число и выдает являеться ли число четным
 // (делиться ли оно на два без остатка). 
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+Console.WriteLine("число четное");
+else 
+Console.WriteLine("число не четное");
